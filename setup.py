b8673c2cfd6ff29d06774df431f5326c86915655
Anthony Federico <dephoona@gmail.com>
2017-01-22 16:21:41 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='tableize',
+      version='v1.0',
+      description='Turn lists into tables with ease',
+      author='Anthony Federico',
+      author_email='dephoona@gmail.com',
+      url='https://github.com/anfederico/Tableize',
+      packages=['tableize']
+     )
